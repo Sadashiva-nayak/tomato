4acdba14a16754ecce5c6371eab8a1170b3d651a
Sadashiva-nayak <sadashivanayak17@gmail.com>
2024-05-25 23:17:16 +0530

    newchange

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://sadashivanayak17:<password>@cluster0.qg9ew8t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI = "mongodb+srv://sadashivanayak17:sada@cluster0.qg9ew8t.mongodb.net/food-delivery"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
