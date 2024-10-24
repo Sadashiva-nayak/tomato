475ce3fb5c2370ef97bfbe695018ec13728925f6
Sadashiva-nayak <sadashivanayak17@gmail.com>
2024-10-24 15:01:28 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022'  // Specify ES2022 or later to support top-level await
+  }
 })
